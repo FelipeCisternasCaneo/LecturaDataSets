2606a995f8284f0486d53b3cac5782cb158d9148
FelipeCisternasCaneo <felipe.cisternas@pucv.cl>
2023-07-01 18:48:52 -0400

    primera subida

---
+++ lectura.py
@@ -0,0 +1,6 @@
+from Problem.FS.Problem import FeatureSelection as fs
+
+# instancias utilizadas
+instancia = ['ionosphere', 'sonar', 'Divorce', 'breast-cancer-wisconsin', 'wdbc']
+
+instance = fs(instancia)
